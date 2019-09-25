dda13500d8baf1f0bb6f3eafe7b6387967934cfa
JoshTiffany <47202503+JoshTiffany@users.noreply.github.com>
2019-09-24 19:05:34 -0500

    Imported ads.

--- app/src/main/java/io/github/joshtiffany/ultimatecalculator/Calculators/MainActivity.java
+++ app/src/main/java/io/github/joshtiffany/ultimatecalculator/Calculators/MainActivity.java
@@ -2,17 +2,21 @@ package io.github.joshtiffany.ultimatecalculator.Calculators;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.material.navigation.NavigationView;
 
 import java.text.DecimalFormat;
 
@@ -66,6 +70,7 @@ public class MainActivity extends AppCompatActivity
     private double val1 = Double.NaN;
     private double val2;
     private char ACTION;
+    private AdView adView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -363,6 +368,10 @@ public class MainActivity extends AppCompatActivity
         decimal = findViewById(R.id.decimalBTN);
         percent = findViewById(R.id.percentBTN);
         addAndMinus = findViewById(R.id.subAndAddBTN);
+
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
     }
 
     private void compute() {
--- app/src/main/java/io/github/joshtiffany/ultimatecalculator/TestActivity.java
+++ app/src/main/java/io/github/joshtiffany/ultimatecalculator/TestActivity.java
@@ -1,11 +1,13 @@
 package io.github.joshtiffany.ultimatecalculator;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class TestActivity extends AppCompatActivity {
 
